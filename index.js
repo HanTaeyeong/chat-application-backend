efea62d263398cc6751ade92c48e26b3afbda67b
한태영 <hantaeyeong@hantaeyeong-ui-MacBookPro.local>
2022-02-06 01:27:43 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+
+const http = require('http')
+const router = require('./router')
+
+const { Server } = require('socket.io')
+
+const cors =require('cors')
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app)
+
+const io = new Server(server)
+
+const corsPolicy={
+    //Headers:'Access-Control-Allow-Headers: X-Requested-With, privatekey',
+    origin:'*',
+    credentials:true
+}
+
+app.use(cors())
+app.use(router);
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room)
+
+        const error = true;
+  
+        if (error) {
+            callback(()=>alert('error'));
+        }
+
+    })
+
+
+    socket.on('disconnect', () => {
+        console.log('user has left')
+    })
+
+})
+
+server.listen(PORT, () => console.log(`server started on ${PORT}`));
+
