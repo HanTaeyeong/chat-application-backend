f09401021562fb443f957ffb8bd39388923b26a3
한태영 <hantaeyeong@hantaeyeong-ui-MacBookPro.local>
2022-02-06 19:49:50 +0900

    cors on socket

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app)
 
-const io = new Server(server)
+const io = new Server(server,{cors:{
+    origin:'*'
+}})
 
 const corsPolicy={
     //Headers:'Access-Control-Allow-Headers: X-Requested-With, privatekey',
