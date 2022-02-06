18398f97a14000449c0b3a066b6815d6d1505050
한태영 <hantaeyeong@hantaeyeong-ui-MacBookPro.local>
2022-02-06 20:17:48 +0900

    socket io user and room

---
+++ users.js
@@ -0,0 +1,29 @@
+let users = []
+
+const addUser = ({ id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    const existingUser = user.find((user) => user.room === room && user.name === name);
+
+    if (existingUser) {
+        return { error: 'Username already exists!' }
+    }
+    const user = { id, name, room }
+    users.push(user)
+    return { user }
+}
+
+const removeUser=(id)=>{
+    return users=users.filter(user=>user.id !== id)
+}
+
+const getUser=(id)=>users.find((user)=>user.id===id)
+
+const getUsersInRoom=(room)=>users.filter((user)=>user.room===room)
+
+
+module.exports ={addUser,removeUser,getUser, getUsersInRoom}
+
+
+
