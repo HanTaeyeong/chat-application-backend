dd9ff50915059cd7b0f71b8dd6ca622c1f5916d2
한태영 <hantaeyeong@hantaeyeong-ui-MacBookPro.local>
2022-02-08 22:04:32 +0900

    socket-functions refactorying

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const router = require('./router')
 const { Server } = require('socket.io')
 
 const cors = require('cors')
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
 
-//const { onConnection } = require('./io-functions')
+//const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
+const { onConnection } = require('./io-functions')
 
 const PORT = process.env.PORT || 5000;
 
@@ -30,41 +31,7 @@ const corsPolicy = {
 app.use(cors())
 app.use(router);
 
-io.on('connection', (socket) => {
-
-    console.log('a user connected')
-
-    socket.on('join', ({ name, room }) => {
-        console.log(name, room)
-        const { error, user } = addUser({ id: socket.id, name, room });
-
-        if (error) return error;
-
-        socket.emit('message', { user: 'admin', text: `${user.name} welcome to the ${room}` })
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` })
-        socket.join(user.room);
-
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
-    })
-
-    socket.on('sendMessage', (message) => {
-        const user = getUser(socket.id)
-        if(!user) return;
-
-        io.to(user.room).emit('meessage', { user: user.name, text: message })
-    })
-
-    socket.on('disconnect', () => {
-        const user = getUser(socket.id)
-
-        if(!user) return;
-
-        io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` })
-        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-        removeUser(socket.id)
-    })
-})
+io.on('connection',onConnection(io))
 
 server.listen(PORT, () => console.log(`server started on ${PORT}`));
 
